b64909e850062449dc9b2e6ea72f884e890a4630
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-02 09:51:36 +0300

    feat: add basic components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import Navbar from 'components/Navbar';
+import CartContainer from 'components/CartContainer';
 
-const App = () => (
-  <div className="app"></div>
+import products from 'static/products';
+
+const App: FunctionComponent = () => (
+  <div className="app">
+    <Navbar />
+    <CartContainer cart={products} />
+  </div>
 );
 
 export default App;
