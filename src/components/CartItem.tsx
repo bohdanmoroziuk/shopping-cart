5795e81ddafefabdf2c59e21241fde089fddfa8e
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-05 16:16:47 +0300

    chore: add icon components

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { Product } from "types";
-
+import ArrowUpIcon from 'icons/ArrowUp'
+import ArrowDownIcon from 'icons/ArrowDown'
 export interface CartItemProps {
   item: Product,
   increase: Function
@@ -23,17 +24,13 @@ const CartItem: FunctionComponent<CartItemProps> = ({ item, increase, decrease, 
       <div>
         {/* increase amount */}
         <button className="amount-btn" onClick={() => increase()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
-          </svg>
+          <ArrowUpIcon />
         </button>
         {/* amount */}
         <p className="amount">{amount}</p>
         {/* decrease amount */}
         <button className="amount-btn" onClick={() => decrease()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
+          <ArrowDownIcon />
         </button>
       </div>
     </div>
