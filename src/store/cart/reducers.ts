a5b9505862ddfda1456760746650c2f62d7be367
Bogdan <bogdan7morozyuk@gmail.com>
2020-08-02 10:29:05 +0300

    refactor(store.cart): rewrite the reducer using immer

--- src/store/cart/reducers.ts
+++ src/store/cart/reducers.ts
@@ -1,44 +1,42 @@
 import { CLEAR_CART, INCREASE, DECREASE, REMOVE } from 'store/cart/types'
 import { CartState, CartActionTypes } from 'store/cart/types'
 import products from 'static/products'
+import produce from 'immer';
+import { Product, ProductId } from 'types';
 
 const initialState: CartState = {
   items: products
 }
 
-const cartReducer = (state = initialState, action: CartActionTypes): CartState => {
-  switch (action.type) {
-    case CLEAR_CART:
-      return {
-        ...state,
-        items: []
+const findIndexById = <T extends { id: TId }, TId>(items: T[]) => (id: TId): number => 
+  items.findIndex(item => item.id === id)
+
+const cartReducer = (state: CartState = initialState, action: CartActionTypes) => {
+  return produce(state, draft => {
+    const findItemIndexById = findIndexById<Product, ProductId>(draft.items)
+
+    switch(action.type) {
+      case CLEAR_CART: {
+        draft.items = [];
+        break;
       }
-    case INCREASE:
-      return {
-        ...state,
-        items: state.items.map(item => (
-          item.id === action.payload.id
-            ? { ...item, amount: item.amount + 1 }
-            : item
-        ))
+      case INCREASE: {
+        const index = findItemIndexById(action.payload.id);
+        index !== -1 && draft.items[index].amount++;
+        break;
       }
-    case DECREASE:
-      return {
-        ...state,
-        items: state.items.map(item => (
-          item.id === action.payload.id && item.amount > 1
-            ? { ...item, amount: item.amount - 1 }
-            : item
-        ))
+      case DECREASE: {
+        const index = findItemIndexById(action.payload.id);
+        (index !== -1 && draft.items[index].amount > 1) && draft.items[index].amount--;
+        break;
       }
-    case REMOVE: 
-      return {
-        ...state,
-        items: state.items.filter(item => item.id !== action.payload.id)
+      case REMOVE: {
+        const index = findItemIndexById(action.payload.id);
+        (index !== -1) && draft.items.splice(index, 1);
+        break;
       }
-    default:
-      return state
-  }
+    }
+  })
 }
 
 export default cartReducer
